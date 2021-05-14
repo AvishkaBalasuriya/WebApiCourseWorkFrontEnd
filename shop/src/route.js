15b3f65d0711c31b2a88c42d11d9b6f06a1f432f
AvishkaBalasuriya <avishkabalasuriya980330@gmail.com>
2021-05-14 21:47:42 +0530

    Basic configs set for project

--- shop/src/route.js
+++ shop/src/route.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const SignUp1 = React.lazy(() => import('./Demo/Authentication/SignUp/SignUp1'));
-const Signin1 = React.lazy(() => import('./Demo/Authentication/SignIn/SignIn1'));
-const ShopHome = React.lazy(() => import('./component/ShopHome'));
+const SignUp1 = React.lazy(() =>
+  import("./Demo/Authentication/SignUp/SignUp1")
+);
+const Signin1 = React.lazy(() =>
+  import("./Demo/Authentication/SignIn/SignIn1")
+);
+const OTPVerification = React.lazy(() =>
+  import("./Demo/Authentication/SignIn/OTPVerfication")
+);
+const Register = React.lazy(() => import("./component/User/Register"));
+const ForgetPassword = React.lazy(() => import("./Demo/Authentication/SignIn/ForgetPassword"));
 const route = [
-    { path: '/auth/signup-1', exact: true, name: 'Signup 1', component: SignUp1 },
-    { path: '/', exact: true, name: 'Signin', component: Signin1 },
-    
+  { path: "/auth/signup-1", exact: true, name: "Signup 1", component: SignUp1 },
+  { path: "/login", exact: true, name: "Signin", component: Signin1 },
+  { path: "/register", exact: true, name: "register", component: Register },
+  { path: "/forget-password", exact: true, name: "register", component: ForgetPassword },
 ];
-
 export default route;
